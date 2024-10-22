d84777eaa2689bea9629dc5df47b19f0939c80d0
mcottle1 <kenzcottle@gmail.com>
2024-10-22 11:53:08 -0600

    reverting test removal

--- src/service/httpPizzaService.ts
+++ src/service/httpPizzaService.ts
@@ -52,7 +52,6 @@ class HttpPizzaService implements PizzaService {
     const { user, token } = await this.callEndpoint('/api/auth', 'POST', { name, email, password });
     localStorage.setItem('user', JSON.stringify(user));
     localStorage.setItem('token', token);
-    console.log(pizzaServiceUrl);
     return Promise.resolve(user);
   }
 
